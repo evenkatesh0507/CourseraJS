bbc47f89c8a3200c3f84cfd900dab173a7fe8818
evenkatesh0507 <evenkatesh0507@gmail.com>
2024-07-02 19:47:54 -0400

    commit 1.0.3

--- Sample Folder/calculate_Area.js
+++ Sample Folder/calculate_Area.js
@@ -4,8 +4,8 @@ let width;
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
-
+   
     let area = length * width;
-}
 
-document.getElementById('result').innerText = 'The area of rectangle is: ${area}';
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
